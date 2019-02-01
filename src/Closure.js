469f57b9f1bc17f40b9ae9c4bb79ffbca2d5f888
munnangi <munnangi.prasanna@gmail.com>
2019-02-01 09:49:24 -0500

    js concepts

---
+++ src/Closure.js
@@ -0,0 +1,12 @@
+// the abilility of inner function to access outer functions variables & parameters is known as closure.
+// even after the execution of the function the inner function has access to the outer functions variables and parameters
+
+var outerFunc = function(a, b) {
+    var c = '!';
+    var innerFunc = function() {
+       console.log(a + b + c);
+    };
+    innerFunc();
+}
+
+outerFunc('Hello', 'World');
