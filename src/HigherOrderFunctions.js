f84aaff2a3a87d701eea77bf271be57916225307
munnangi <munnangi.prasanna@gmail.com>
2019-03-24 23:57:46 -0400

    hof

--- src/HigherOrderFunctions.js
+++ src/HigherOrderFunctions.js
@@ -5,3 +5,8 @@ const discounter = discount => price => price * (1 - discount);
 
 const tenPercentOff = discounter(0.1);
 console.log(tenPercentOff(100)); // 90
+
+// you can also call one function after the other by just
+// adding the second set of parameters in the parentheses right after the first
+
+discounter(0.1)(100);
